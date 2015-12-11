e79df3150450816f7bfe2ceab988c0bcae9e993f
David Yoon <david_yoon86@hotmail.com>
2015-12-11 04:09:52 -0500

    chips

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-(function(){	
+(function(){
+	var deck;
 
+	// Constructor function to make cards
 	function Card (value, image, type){
 		this.value = value,
 		this.image = image,
 		this.type = type
 	}
 
-	function createDeck(){
+	// Make deck of cards in order
+	function unshuffledDeck(){
 		var deck = []
 		var value = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, "A", "A", "A", "A"]
-		var img = ["2_of_diamonds.png", "2_of_clubs.png", "2_of_hearts.png", "2_of_spades2.png", "3_of_diamonds.png", "3_of_clubs.png", "3_of_hearts.png", "3_of_spades2.png", "4_of_diamonds.png", "4_of_clubs.png", "4_of_hearts.png", "4_of_spades2.png", "5_of_diamonds.png", "5_of_clubs.png", "5_of_hearts.png", "5_of_spades2.png", "6_of_diamonds.png", "6_of_clubs.png", "6_of_hearts.png", "6_of_spades2.png", "7_of_diamonds.png", "7_of_clubs.png", "7_of_hearts.png", "7_of_spades2.png", "8_of_diamonds.png", "8_of_clubs.png", "8_of_hearts.png", "8_of_spades2.png", "9_of_diamonds.png", "9_of_clubs.png", "9_of_hearts.png", "9_of_spades2.png", "10_of_diamonds.png", "10_of_clubs.png", "10_of_hearts.png", "10_of_spades2.png", "jack_of_diamonds.png", "jack_of_clubs.png", "jack_of_hearts.png", "jack_of_spades2.png", "queen_of_diamonds.png", "queen_of_clubs.png", "queen_of_hearts.png", "queen_of_spades2.png", "king_of_diamonds.png", "king_of_clubs.png", "king_of_hearts.png", "king_of_spades2.png", "ace_of_diamonds.png", "ace_of_clubs.png", "ace_of_hearts.png", "ace_of_spades2.png"]
-		for(var i = 0; i < 10; i ++){queen
+		var img = ["2_of_diamonds.png", "2_of_clubs.png", "2_of_hearts.png", "2_of_spades.png", "3_of_diamonds.png", "3_of_clubs.png", "3_of_hearts.png", "3_of_spades.png", "4_of_diamonds.png", "4_of_clubs.png", "4_of_hearts.png", "4_of_spades.png", "5_of_diamonds.png", "5_of_clubs.png", "5_of_hearts.png", "5_of_spades.png", "6_of_diamonds.png", "6_of_clubs.png", "6_of_hearts.png", "6_of_spades.png", "7_of_diamonds.png", "7_of_clubs.png", "7_of_hearts.png", "7_of_spades.png", "8_of_diamonds.png", "8_of_clubs.png", "8_of_hearts.png", "8_of_spades.png", "9_of_diamonds.png", "9_of_clubs.png", "9_of_hearts.png", "9_of_spades.png", "10_of_diamonds.png", "10_of_clubs.png", "10_of_hearts.png", "10_of_spades.png", "jack_of_diamonds.png", "jack_of_clubs.png", "jack_of_hearts.png", "jack_of_spades.png", "queen_of_diamonds.png", "queen_of_clubs.png", "queen_of_hearts.png", "queen_of_spades.png", "king_of_diamonds.png", "king_of_clubs.png", "king_of_hearts.png", "king_of_spades.png", "ace_of_diamonds.png", "ace_of_clubs.png", "ace_of_hearts.png", "ace_of_spades2.png"]
+		for(var i = 0; i < 52; i ++){
 
 			var t = new Card(value[i], img[i])
-			array.push(t)
+			deck.push(t)
+
 		}
+		return deck
 	}
+
+	// FISHER-YATES SHUFFLE
+	function shuffle(array){
+		var shuffled = [], n = array.length, i;
+		while(n){
+			i = Math.floor(Math.random() * n--);
+			shuffled.push(array.splice(i, 1)[0]);
+		}
+		return shuffled
+	}
+
+	$(".bj-deal").on("click", function(){
+		var bet = Number($(".bj-bet-amount").html());
+		var toShuffle = unshuffledDeck();
+		deck = shuffle(toShuffle);
+	})
+
+
+
+	// Update value of bet and balance while checking for funds/max
+	$(".bj-chip").on("click", function(){
+		var chip = $(event.target).prop("id").split("p");
+		var value = Number(chip[1])
+		var currentBet = Number($(".bj-bet-amount").html());
+		var currentBank = Number($(".bj-bank-amount").html());
+		
+		if(currentBet + value <= 2000 && currentBank - value >= 0){
+			$(".bj-bet-amount").html(currentBet + value);
+			$(".bj-bank-amount").html(currentBank - value);
+		}else if(currentBank - value < 0){
+			swal({ title: "Insufficient funds!", text: "Try making smaller bets.", timer: 2000, showConfirmButton: false });
+		}else{
+			swal({ title: "Maximum bet exceeded!", text: "Maximum bet is $2000.", timer: 2000, showConfirmButton: false });
+		}
+	});
+
+
 
 })();
